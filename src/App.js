9e768e1835344c2d47c38a5130fb77e8e7cc3636
Anna Lin <annaaa@Annas-Mac-n-Cheese.local>
2018-08-15 19:21:57 -0700

    Calling data, now componentizingaoksd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Data scraped from https://cuesa.org/eat-seasonally/charts/vegetables
+import data from './beautifulveggiesdata.json';
+
+const currentMonth = 9;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      veggies: []
+    }
+  }
+
+  componentDidMount() {
+    this.getVeggies();
+  }
+
+  getVeggies() {
+    let veggies = [];
+
+    data.forEach(function(item) {
+      console.log(item);
+      if (item.months[currentMonth]) {
+        veggies.push(item.name);
+      }
+    })
+
+    this.setState({
+      veggies: veggies
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +42,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.veggies}
         </p>
       </div>
     );
